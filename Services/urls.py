83da0e6651ceb8ef1d64fd80e38f63a9e905630d
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2023-03-17 07:27:29 +0000

    updated the path indentation

--- Services/urls.py
+++ Services/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('service/', views.service,name="service"),
     path("add/",views.add_bookings,name="add"),
-     path("display/",views.displayBookings,name="display"),
+    path("display/",views.displayBookings,name="display"),
     # path('courseDetail/', views.courseDetail,name="courseDetail"),
     # path('moduleDetail/', views.moduleDetail,name="moduleDetail"),
     # path('listing/', views.listing,name="listing"),
